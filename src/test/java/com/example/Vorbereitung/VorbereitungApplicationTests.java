a7d245a1badf0c9a3630bc9ff48e703034d80314
Jelena Kocic <wi22b087@technikum-wien.at>
2023-06-28 18:03:21 +0200

    Initial commit

---
+++ src/test/java/com/example/Vorbereitung/VorbereitungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Vorbereitung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VorbereitungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
